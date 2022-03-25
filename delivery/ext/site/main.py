2b83c45025697f9d95d751a32e82a6f5ec3b6ece
Glayton Martins <gr.martins@outlook.com>
2022-03-25 11:41:30 -0300

    Adding templates and debug toolbar

--- delivery/ext/site/main.py
+++ delivery/ext/site/main.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint
+from flask import render_template
 
 bp = Blueprint("site", __name__)
 
@@ -6,3 +7,13 @@ bp = Blueprint("site", __name__)
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
+
+
+@bp.route("/restaurantes")
+def restaurants():
+    return render_template("restaurants.html")
